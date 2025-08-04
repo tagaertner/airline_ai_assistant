dd6abfdd5cdff6ae80e1bb05e96d4a6abccfdea5
Tami <tamigaertner@outlook.com>
2025-08-04 11:38:35 -0400

    work on audio and img

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import json
 from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr 
+import base64
+from io import BytesIO
+from PIL import Image
+from pydub import AudioSegment
+from pydub.playback import play
 
 load_dotenv(override=True)
 
@@ -33,9 +38,35 @@ price_function = {
 ticket_prices = {"london": "$799", "paris": "$899", "tokyo": "$1400", "berlin": "$499"}
 tools = [{"type": "function", "function": price_function}]
 
-def chat(message, history):
-    messages = [{"role":"system", "content": message}] + history + [{"role": "user", "content": message}]
+def artist(city):
+    image_response = openai.images.generate(
+        model="dall-e-3",
+        prompt=f"An image representing a vacation in{city}, showing tourist spots and everything unique about {city} in a  Rembrandt style ",
+        size="1024x1024", # smallest size for dall-e
+        n=1,
+        response_format="b64_json",
+    )
+    image_base64 = image_response.data[0].b64_json
+    image_data = base64.b64decode(image_base64)
+    return Image.open(BytesIO(image_data))
+
+def talker(message):
+    response = openai.audio.speech.create(
+        model="tts-1",
+        voice="onyx", # or alloy
+        input=message
+        
+    )
+    
+    audio_stream = BytesIO(response.content)
+    audio = AudioSegment.from_file(audio_stream, format="mp3")
+    play(audio)
+  
+
+def chat(history):
+    messages = [{"role":"system", "content": system_message}] + history 
     response = openai.chat.completions.create(model=gpt_model, messages=messages, tools=tools)
+    image = None
     
     # This is what comes back from the LLM
     # user , ass, user, asss
@@ -44,9 +75,18 @@ def chat(message, history):
         response, city = handle_tool_call(message) # unpack the messsage from gpt
         messages.append(message) # must add 2 new roles. the message that we got back from gtp
         messages.append(response)
+        image = artist(city)
         response = openai.chat.completions.create(model=gpt_model, messages=messages)
         
-    return response.choices[0].message.content
+    reply = response.choices[0].message.content 
+    history += [{"role": "assistant", "content":reply}]
+    
+    # audio
+    talker(reply)
+    
+    return history, image
+        
+    # return response.choices[0].message.content
 
 def get_ticket_price(destination_city):
     print(f"Too get_ticket_price called for {destination_city}")
@@ -65,23 +105,27 @@ def handle_tool_call(message):
         }
     return response, city
 
-gr.ChatInterface(fn=chat, type="messages").launch()
-# def chat(message, history):
-#     messages =[{"role": "system", "content": system_message}] + history +[{"role":"user", "content": message}]
-#     response = openai.chat.completions.create(model=gpt_model, messages=messages)
-    
 # gr.ChatInterface(fn=chat, type="messages").launch()
 
-# ticket_prices = {"london": "$799", "paris": "$899", "tokyo": "$1400", "berlin": "$499"}
+with gr.Blocks() as ui:
+    with gr.Row():
+        chatbot = gr. Chatbot(height=500, type="messages")
+        image_output = gr.Image(height=500)
+    with gr.Row():
+        entry = gr.Textbox(label="Chat with our AI Assistant:")
+    with gr.Row():
+        clear = gr.Button("Clear")
+
+    def do_entry(message, history):
+        history += [{"role": "user", "content": message}]
+        return "", history
+
+    entry.submit(do_entry, inputs=[entry, chatbot], outputs=[entry, chatbot]).then(
+        chat, inputs=[chatbot], outputs=[chatbot, image_output]
+    )
+    clear.click(lambda: None, inputs=None, outputs=chatbot, queue=False)
+ui.launch(inbrowser=True)
 
 
-
-
-
-
-
-# # And this is included in a list of tools:
-
-# tools = [{"type": "function", "function": price_function}]
 
  
